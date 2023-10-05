8b5ab0f813effcf78349919b3108f6f68ac5881f
Bishnu <thapabishnu20@gmail.com>
2023-10-05 15:27:04 +0545

    managing state in react(active class on click)

--- react-app-typescript/src/components/ListGroup.tsx
+++ react-app-typescript/src/components/ListGroup.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+
 function ListGroup() {
   const items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  // using the useState hook we can tell the react app that this component can have data or state that will change overtime.
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
@@ -7,9 +11,13 @@ function ListGroup() {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log(item, index)}
+            onClick={() => setSelectedIndex(index)}
           >
             {item}
           </li>
