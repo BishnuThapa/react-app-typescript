6c532e014e3b1caaceea09ef38c820a0c7866321
Bishnu <thapabishnu20@gmail.com>
2023-10-05 14:01:20 +0545

    conditional rendering that means display no items if list is empty

--- react-app-typescript/src/components/ListGroup.tsx
+++ react-app-typescript/src/components/ListGroup.tsx
@@ -1,8 +1,10 @@
 function ListGroup() {
   const items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+
   return (
     <>
-      <h1>List Group</h1>
+      <h1>List</h1>
+      {items.length === 0 && <h2>No Items Found</h2>}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
