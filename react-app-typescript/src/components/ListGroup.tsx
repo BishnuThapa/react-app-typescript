2a958444b703607120727b963b109c7393f6044d
Bishnu <thapabishnu20@gmail.com>
2023-10-05 15:15:09 +0545

    handling events in react

--- react-app-typescript/src/components/ListGroup.tsx
+++ react-app-typescript/src/components/ListGroup.tsx
@@ -1,13 +1,16 @@
 function ListGroup() {
   const items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <h2>No Items Found</h2>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
             {item}
           </li>
         ))}
