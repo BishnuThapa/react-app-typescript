0153cd2b3194db385069990f64d774443773a217
Bishnu <thapabishnu20@gmail.com>
2023-10-06 14:33:25 +0545

    show & hide dismissable alert when clicking button using usestate

--- react-app-typescript/src/App.tsx
+++ react-app-typescript/src/App.tsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   const items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  const [alertVisible, setAlertVisiable] = useState(false);
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
   return (
     <>
-      <Button onClick={() => console.log("Clicked")}>Shop more </Button>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisiable(false)}>
+          Hello <span>World</span>
+        </Alert>
+      )}
+
+      <Button onClick={() => setAlertVisiable(true)}>Button</Button>
+
       <ListGroup
         items={items}
         heading="Cities"
