7b9c2a78fea21138de914481140148abffd59512
Bishnu <thapabishnu20@gmail.com>
2023-10-07 21:23:35 +0545

    building a like component

--- react-app-typescript/src/App.tsx
+++ react-app-typescript/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import Like from "./components/Like";
 
 function App() {
   const items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
@@ -18,7 +19,7 @@ function App() {
       )}
 
       <Button onClick={() => setAlertVisiable(true)}>Button</Button>
-
+      <Like onClick={() => console.log("Clicked")} />
       <ListGroup
         items={items}
         heading="Cities"
